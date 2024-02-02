e88367af3e4612796c26f86de55fd62a679e7d6b
Machaieie <machaieieedwin1@gmail.com>
2024-02-02 12:43:14 +0200

    calculo dos livros disponivei na base de dados

--- src/main/java/com/livrariamabuko/Livraria/Mabuko/service/StockService.java
+++ src/main/java/com/livrariamabuko/Livraria/Mabuko/service/StockService.java
@@ -1,9 +1,11 @@
 package com.livrariamabuko.Livraria.Mabuko.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.livrariamabuko.Livraria.Mabuko.repository.StockRepository;
 
+@Service
 public class StockService {
     
     @Autowired
